d5100b379f69061028452e2904796c68db50aac4
BrainStarr <brian.starr@airbnb.com>
2014-08-31 15:09:09 -0700

    Initial Commit

---
+++ 99 bottlesTests/_9_bottlesTests.h
@@ -0,0 +1,13 @@
+//
+//  _9_bottlesTests.h
+//  99 bottlesTests
+//
+//  Created by Brian Starr on 8/31/14.
+//  Copyright (c) 2014 Brian Starr. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface _9_bottlesTests : SenTestCase
+
+@end
---
+++ 99 bottlesTests/_9_bottlesTests.m
@@ -0,0 +1,32 @@
+//
+//  _9_bottlesTests.m
+//  99 bottlesTests
+//
+//  Created by Brian Starr on 8/31/14.
+//  Copyright (c) 2014 Brian Starr. All rights reserved.
+//
+
+#import "_9_bottlesTests.h"
+
+@implementation _9_bottlesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in 99 bottlesTests");
+}
+
+@end
